5e4c048e0ead6b713892cd8b5a9afbafb9954337
damiano.lozzi <damiano.lozzi@dgsspa.com>
2025-03-27 09:55:23 +0100

    PN-14367: solved sonar issues.

--- src/main/java/it/pagopa/pn/ss/dummy/sign/service/PnDummySignServiceImpl.java
+++ src/main/java/it/pagopa/pn/ss/dummy/sign/service/PnDummySignServiceImpl.java
@@ -5,7 +5,6 @@ import it.pagopa.pn.library.exceptions.PnSpapiPermanentErrorException;
 import it.pagopa.pn.library.sign.pojo.PnSignDocumentResponse;
 import it.pagopa.pn.ss.dummy.sign.pojo.SignatureFormat;
 import it.pagopa.pn.library.sign.service.PnSignService;
-import lombok.AllArgsConstructor;
 import lombok.CustomLog;
 import lombok.NoArgsConstructor;
 import reactor.core.publisher.Mono;
@@ -14,10 +13,7 @@ import java.util.UUID;
 
 @CustomLog
 @NoArgsConstructor
-@AllArgsConstructor
 public class PnDummySignServiceImpl implements PnSignService {
-
-    private String apiEndpoint;
 
     /**
      * Sign a PDF document with PAdES format
@@ -59,11 +55,11 @@ public class PnDummySignServiceImpl implements PnSignService {
      * @param fileBytes the document to sign
      * @return a {@link PnSignDocumentResponse} with the signed document
      */
-    private Mono<PnSignDocumentResponse> applySignature(String format, Boolean timestamping, byte[] fileBytes) {
+    private Mono<PnSignDocumentResponse> applySignature(SignatureFormat format, boolean timestamping, byte[] fileBytes) {
         if(fileBytes == null || fileBytes.length == 0) {
             return Mono.error(new PnSpapiPermanentErrorException("fileBytes cannot be null or empty"));
         }
-        String level = timestamping ? SignatureLevel.TIMESTAMP : SignatureLevel.BASIC;
+        String level = timestamping ? SignatureLevel.TIMESTAMP.getValue() : SignatureLevel.BASIC.getValue();
         var requestId = UUID.randomUUID().toString();
         String message = "Dummy - Invoked [sign{}] request {} with params format={}, level={}, requestBody length: {} bytes.";
         log.info(message, format, requestId, format, level, fileBytes.length);
