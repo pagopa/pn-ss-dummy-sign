2b4569f1b06dd98a86b4488b5a51873ce919f776
damiano.lozzi <damiano.lozzi@dgsspa.com>
2025-03-31 18:28:20 +0200

    PN-14367: added configurable delay.

--- src/main/java/it/pagopa/pn/ss/dummy/sign/service/PnDummySignServiceImpl.java
+++ src/main/java/it/pagopa/pn/ss/dummy/sign/service/PnDummySignServiceImpl.java
@@ -7,13 +7,25 @@ import it.pagopa.pn.ss.dummy.sign.pojo.SignatureFormat;
 import it.pagopa.pn.library.sign.service.PnSignService;
 import lombok.CustomLog;
 import lombok.NoArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import reactor.core.publisher.Mono;
 
 import java.util.UUID;
 
+import static it.pagopa.pn.ss.dummy.sign.util.DummyPecUtils.delayElement;
+
 @CustomLog
 @NoArgsConstructor
 public class PnDummySignServiceImpl implements PnSignService {
+
+    @Value("${dummy.sign.base-delay:100}")
+    private double baseDelay;
+    @Value("${dummy.sign.file-size-scale:1000}")
+    private double fileSizeScale;
+    @Value("${dummy.sign.min-scaling:1}")
+    private double minScaling;
+    @Value("${dummy.sign.max-scaling:1.5}")
+    private double maxScaling;
 
     /**
      * Sign a PDF document with PAdES format
@@ -56,14 +68,15 @@ public class PnDummySignServiceImpl implements PnSignService {
      * @return a {@link PnSignDocumentResponse} with the signed document
      */
     private Mono<PnSignDocumentResponse> applySignature(SignatureFormat format, boolean timestamping, byte[] fileBytes) {
-        if(fileBytes == null || fileBytes.length == 0) {
-            return Mono.error(new PnSpapiPermanentErrorException("fileBytes cannot be null or empty"));
+        if (fileBytes == null || fileBytes.length == 0) {
+            return Mono.delay(delayElement(baseDelay, maxScaling))
+                    .then(Mono.error(new PnSpapiPermanentErrorException("fileBytes cannot be null or empty")));
         }
         String level = timestamping ? SignatureLevel.TIMESTAMP.getValue() : SignatureLevel.BASIC.getValue();
         var requestId = UUID.randomUUID().toString();
         String message = "Dummy - Invoked [sign{}] request {} with params format={}, level={}, requestBody length: {} bytes.";
         log.info(message, format, requestId, format, level, fileBytes.length);
-        return Mono.just(new PnSignDocumentResponse(fileBytes));
+        return Mono.delay(delayElement(fileBytes.length, baseDelay, fileSizeScale, minScaling, maxScaling)).then(Mono.just(new PnSignDocumentResponse(fileBytes)));
     }
 
 }
